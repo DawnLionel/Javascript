16abbffab8ca3269a1a96f7235b40bee0c9eb045
DawnLionel <819473753@qq.com>
2019-10-27 01:21:37 +0800

    add vue_idCard

---
+++ js/constellation.js
@@ -0,0 +1,7 @@
+var constellations = [
+    {"Start":121,"End":220,"Name":"水平座"},    {"Start":221,"End":320,"Name":"双鱼座"},
+    {"Start":321,"End":420,"Name":"白羊座"},    {"Start":421,"End":520,"Name":"金牛座"},
+    {"Start":521,"End":620,"Name":"双子座"},    {"Start":621,"End":720,"Name":"巨蟹座"},
+    {"Start":721,"End":820,"Name":"狮子座"},    {"Start":821,"End":920,"Name":"处女座"},
+    {"Start":921,"End":1020,"Name":"天秤座"},       {"Start":1021,"End":1120,"Name":"天蝎座"},
+    {"Start":1121,"End":1220,"Name":"射手座"}    ];
